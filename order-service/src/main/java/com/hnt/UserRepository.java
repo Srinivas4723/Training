72606c5e22364ba480ea495b5e203a00b2ade3dc
unknown <cogjava3164@iiht.tech>
2022-08-22 17:35:26 +0530

    day1

---
+++ order-service/src/main/java/com/hnt/UserRepository.java
@@ -0,0 +1,10 @@
+package com.hnt;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.hnt.entity.User;
+
+public interface UserRepository extends CrudRepository<User, Integer>{
+
+}
